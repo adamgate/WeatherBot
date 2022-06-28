2eda965f9f906c31bea0578e2768ed6084d6ee4e
adamapplegate <applegateadaml@gmail.com>
2022-06-27 20:45:33 -0600

    Read Discord OAuth token from file

--- WeatherBot/Program.cs
+++ WeatherBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -16,15 +17,11 @@ namespace WeatherBot
             _client = new DiscordSocketClient();
 
             _client.Log += Log;
-
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = "token";
-
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            
+            //Get token from file
+            var token = File.ReadAllText("../../token.txt");
+            
+            Console.WriteLine(token);
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();

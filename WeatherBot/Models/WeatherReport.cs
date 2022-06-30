bf65dad55adada7e2eebc0a897aeae9f0f8edef1
adamapplegate <applegateadaml@gmail.com>
2022-06-30 09:08:58 -0600

    Added OpenWeather API equivalent classes

---
+++ WeatherBot/Models/WeatherReport.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherBot.Models
+{
+    //Root class from OpenWeather JSON
+    public class WeatherReport
+    {
+        public List<Weather> Weather { get; }
+        public Main Main { get; }
+        public int Visibility { get; }
+        public Wind Wind { get; }
+        public Sys Sys { get; }
+        public int Timezone { get; }
+
+    }
+
+    public class Main
+    {
+        public double Temp { get; }
+        public double FeelsLike { get; }
+        public double TempMin { get; }
+        public double TempMax { get; }
+        public int Pressue { get; }
+        public int Humidity { get; }
+    }
+
+    public class Sys
+    {
+        public int Sunrise { get; }
+        public int Sunset { get; }
+    }
+
+    public class Weather
+    {
+        public string Main { get; }
+        public string Description { get; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; }
+        public int deg { get; }
+        public double gust { get; }
+    }
+}
